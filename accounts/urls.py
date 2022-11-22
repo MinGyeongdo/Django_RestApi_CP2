75c8d936a86f15d6b2012eb41e2e706740b1ff97
Mingyeongdo <mgd813@gmail.com>
2022-11-22 09:37:28 +0900

    로그인 구현완

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+# accounts/urls.py
+
+from django.urls import include, path
+from . import views
+
+login_patterns = [
+    path('normal/', views.login, name='login'),
+]
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('login/', include(login_patterns)),
+]
